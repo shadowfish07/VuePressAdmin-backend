c1235a4db7fba54025123e45b75c62e46a871532
shadowfish07 <38303418+shadowfish07@users.noreply.github.com>
2022-04-29 15:05:10 +0800

    feat(config): 现在PATCH /api/config接口会强制要求application/json
    如果不满足，会返回422异常

--- app/controller/config.js
+++ app/controller/config.js
@@ -4,6 +4,9 @@ const Controller = require('egg').Controller;
 class ConfigController extends Controller {
   async patch() {
     const { ctx } = this;
+    if (ctx.request.header['content-type'] !== 'application/json') {
+      return ctx.response.returnFail('content-type必须是application/json', 422);
+    }
     await ctx.service.config.patch(ctx.request.body);
     return ctx.response.returnSuccess();
   }
