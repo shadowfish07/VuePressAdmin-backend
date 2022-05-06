888626660e40fe6dfde6d5e12c7512bd2544bdf0
shadowfish07 <38303418+shadowfish07@users.noreply.github.com>
2022-05-06 21:34:39 +0800

    feat(auth): 在auth中间件写入ctx.userId
    可以被egg-logger读取，或者在其他地方直接使用

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -2,6 +2,7 @@
 module.exports = () => {
   return async function auth(ctx, next) {
     const id = ctx.session.userId;
+    ctx.userId = id;
     if (!id) {
       ctx.logger.info('Unauthorized.cookies-id is null.');
       return ctx.response.returnFail('用户未登录', 401);
