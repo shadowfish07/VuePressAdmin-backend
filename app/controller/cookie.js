84ebb2f3aba53cefef2f6b73dea8c7eac64140fe
shadowfish07 <38303418+shadowfish07@users.noreply.github.com>
2022-05-11 00:33:12 +0800

    docs(controller): 引入apidoc，为所有接口编写apidoc规范，加入apidoc 自动部署action

--- app/controller/cookie.js
+++ app/controller/cookie.js
@@ -4,11 +4,23 @@ const Controller = require('egg').Controller;
 
 class CookieController extends Controller {
   /**
-   * 返回登录态cookie，同时返回用户个人信息
-   * @api GET /cookie
+   * @api {get} /cookie 登录
    * @apiName 登录
-   * @apiParam {String} username 用户名
-   * @apiParam {String} password 密码
+   * @apiGroup Cookie
+   * @apiDescription 返回登录态cookie，同时返回用户个人信息
+   * @apiVersion 0.1.0
+   * @apiPermission 普通用户
+   *
+   * @apiBody {String} username 用户名
+   * @apiBody {String} password 密码
+   *
+   * @apiSuccess {Boolean} success 是否成功
+   * @apiUse UserInfoSuccessData
+   * @apiSuccess {string} errorMessage 错误信息
+   * @apiSuccess {string} traceId 请求id
+   *
+   * @apiError 422 传入参数错误
+   * @apiError 400 用户名或密码错误
    */
   async getCookie() {
     const { ctx } = this;
--- app/controller/user.js
+++ app/controller/user.js
@@ -4,9 +4,17 @@ const Controller = require('egg').Controller;
 
 class UserController extends Controller {
   /**
-   * 返回当前用户个人信息
-   * @api GET /user/me
-   * @apiName 获取个人信息
+   * @api {get} /user/me 获取当前登录用户个人信息
+   * @apiName 获取当前登录用户个人信息
+   * @apiGroup User
+   * @apiDescription 返回当前用户个人信息
+   * @apiVersion 0.1.0
+   * @apiPermission 普通用户
+   *
+   * @apiSuccess {Boolean} success 是否成功
+   * @apiUse UserInfoSuccessData
+   * @apiSuccess {string} errorMessage 错误信息
+   * @apiSuccess {string} traceId 请求id
    */
   async me() {
     this.ctx.response.returnSuccess(
